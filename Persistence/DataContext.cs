c2e708174b12a6e3c3899c131413315740b02f6f
unknown <ritutosh89@gmail.com>
2020-08-24 01:07:18 +0530

    End of Section 4

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿
 using Domain;
 using Microsoft.EntityFrameworkCore;
 namespace Persistence
@@ -10,6 +10,7 @@ namespace Persistence
          {
          }
          public DbSet <Value> Values{ get; set; }
+         public DbSet <Activity> Activities{get;set;}
          protected override void OnModelCreating(ModelBuilder builder)
          {
              builder.Entity<Value>()
@@ -22,3 +23,4 @@ namespace Persistence
          }
     }
 }
+    
